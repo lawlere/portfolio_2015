7bc479187f814f998e4b8d221458c3125c92fc30
Philip I. Thomas <mail@philipithomas.com>
2015-08-07 19:21:23 -0700

    Start with tiles collapsed - closes #20. Also, add buttons to bottom of screen - closes #11

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
     var m,
         config = {
-            // id to number of slides
-            "fitstar": 2,
-            "glassbreakers": 3,
-            "kubmo": 1,
+            // id to data
+            "fitstar": {
+                "num_slides": 2,
+                "button_label": "Go to the FitStar Website",
+                "button_url": "http://fitstar.com/",
+            },
+            "glassbreakers": {
+                "num_slides": 3,
+                "button_label": "Go to the Glassbreakers website",
+                "button_url": "https://www.glassbreakers.co/",
+            },
+            "kubmo": {
+                "num_slides": 3,
+                "button_label": "Go to the Kubmo website",
+                "button_url": "http://www.kubmo.builders/",
+            },
         }
     ;
 
     m = new Mermaid(config);
-    m.init("fitstar"); // Does event binding and stuff
+    m.init(); // Does event binding and stuff
 });
 
