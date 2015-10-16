5ed5ca278235d6c18573da8df9a2c7d190d0fac8
Philip I. Thomas <mail@philipithomas.com>
2015-10-16 13:05:44 -0700

    forgot cloudflare domain!

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ bundle exec s3_website push
 
 # TODO - future when there is cloudflare
 # Clear the Cloudflare cache
-curl https://www.cloudflare.com/api_json.html \
+curl --fail https://www.cloudflare.com/api_json.html \
     -d 'a=fpurge_ts' \
     -d "tkn=$CLOUDFLARE_API_KEY" \
     -d "email=$CLOUDFLARE_EMAIL" \
-    -d "z=$cloudflare_zone" \
+    -d "z=emmalawler.com" \
     -d 'v=1'
