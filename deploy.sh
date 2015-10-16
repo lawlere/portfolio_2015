6831d409ffd1a1e166b23c96eedf6677aae8be87
Philip I. Thomas <mail@philipithomas.com>
2015-10-16 13:02:49 -0700

    closes lawlere/portfolio_2015#36

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,11 @@ set -e
 # Push the build using the s3_website.yml settings
 bundle exec s3_website push
 
-exit 0
-
 # TODO - future when there is cloudflare
 # Clear the Cloudflare cache
 curl https://www.cloudflare.com/api_json.html \
     -d 'a=fpurge_ts' \
-    -d "tkn=$cloudflare_token" \
-    -d "email=$cloudflare_email" \
+    -d "tkn=$CLOUDFLARE_API_KEY" \
+    -d "email=$CLOUDFLARE_EMAIL" \
     -d "z=$cloudflare_zone" \
     -d 'v=1'
